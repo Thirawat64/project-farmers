f143bc6ab63eeb5abbab9f4504715fdf5712fb54
Thirawat64 <thirawat.ka.64@ubu.ac.th>
2023-12-07 18:42:37 +0700

    new

--- farmers/user/urls.py
+++ farmers/user/urls.py
@@ -1,7 +1,15 @@
-from django.urls import path
+from django.urls import path, include
+from django.contrib.auth.views import LoginView, LogoutView
 from .views import *
 
 urlpatterns = [
+    # Your other views and paths go here
     path('', Register, name='register'),
     path('login/', Login, name='login'),
+    # Example: Include Django authentication URLs
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+
+    # You can include more authentication-related URLs as needed
+    path('password_reset/', include('django.contrib.auth.urls')),
 ]
--- farmers/user/views.py
+++ farmers/user/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def Register(req):
-    return render(req, 'user/register.html')
+    return render(req, 'registration/register.html')
 
 def Login(req):
-    return render(req, 'user/login.html')
+    return render(req, 'registration/login.html')
